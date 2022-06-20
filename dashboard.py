8df411a5630f3fb28d0214cbf84dca43bb582a6a
unknowngfonovich <62668689+unknowngfonovich@users.noreply.github.com>
2022-06-20 23:37:30 +0300

    update dash - started to build

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,6 @@
 from dash import Dash, html, dcc
+from matplotlib.axis import YAxis
+from matplotlib.pyplot import xlabel, ylabel
 import plotly.express as px
 import pandas as pd
 
@@ -10,41 +12,52 @@ colors = {
 }
 # layout describes what the applications looks like
 
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+data = pd.read_csv('datasets/hotel_bookings.csv')
+calendar = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
 
-fig = px.bar(df, x='Fruit', y='Amount', color='City', barmode='group')
+df1 = data.groupby(['arrival_date_month', 'hotel'], as_index=False).agg({'arrival_date_year': 'count'}).set_index('arrival_date_month').loc[calendar].reset_index()
+df1.columns = ['arrival_date_month', 'hotel', 'counter']
+
+fig = px.histogram(df1, x='arrival_date_month', y='counter', color='hotel', ylabel='Number of bookings', xlabel='Month of arrival')
 
 
 fig.update_layout(
     plot_bgcolor=colors['background'],
     paper_bgcolor=colors['background'],
-    font_color=colors['text']
+    font_color=colors['text'],
 )
 
 app.layout = html.Div(children=[
     html.H1(
-        children='Hello World',
+        children='Hotel booking',
         style={
             'textAlign': 'center',
             'color': colors['text']
         }
     ),
-    html.Div(
-        children='Dash is web application framework for your data',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
+
     dcc.Graph(
         id='examle-graph',
         figure=fig,
-    )
-])
+    )],
+    style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
